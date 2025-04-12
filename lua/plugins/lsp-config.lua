5e933f8fdab7b123516e6c9565dc713ac0d06eef
Daniel Gonzalez <danielgona2002@gmail.com>
2025-04-12 07:16:47 -0500

    subo cambios al lsp y none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,57 +1,57 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.ts_ls.setup({ capabilities = capabilities })
-      lspconfig.solargraph.setup({ capabilities = capabilities })
-      lspconfig.html.setup({ capabilities = capabilities })
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            staticcheck = true,
-          },
-        },
-      })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.solargraph.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						staticcheck = true,
+					},
+				},
+			})
 
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "■", -- o "»", "→", "■"
-          spacing = 4,
-        },
-        signs = true,
-        underline = false,
-        update_in_insert = false,
-        severity_sort = true,
-      })
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "■", -- o "»", "→", "■"
+					spacing = 4,
+				},
+				signs = true,
+				underline = false,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-      -- Keymaps
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<C-.>", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<C-.>", vim.lsp.buf.definition, { noremap = true, silent = true })
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-    end,
-  },
+			-- Keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<C-.>", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "<C-]>", "<Nop>", { noremap = false, silent = false})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,34 +1,36 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				-- 🧼 FORMATTERS
-				null_ls.builtins.formatting.stylua, -- Lua
-				null_ls.builtins.formatting.prettier, -- JS, TS, HTML, CSS...
-				null_ls.builtins.formatting.gofmt, -- Go
+        null_ls.setup({
+            sources = {
+                -- 🧼 FORMATTERS
+                null_ls.builtins.formatting.stylua, -- Lua
+                null_ls.builtins.formatting.prettier, -- JS, TS, HTML, CSS...
+                null_ls.builtins.formatting.gofmt, -- Go
 
-				-- ✍️ AUTOCOMPLETADO
-				null_ls.builtins.completion.spell, -- Autocompletado de palabras
-			},
-		})
+                -- ✍️ AUTOCOMPLETADO
+                null_ls.builtins.completion.spell, -- Autocompletado de palabras
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+            },
+        })
 
-		-- 🔑 Formatear manualmente con <leader>f
-		vim.keymap.set("n", "<leader>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, {})
+        -- 🔑 Formatear manualmente con <leader>f
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+        end, {})
 
-		-- 💾 Autoformateo al guardar archivos Go
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*.go",
-			callback = function()
-				vim.lsp.buf.format({ async = false })
-			end,
-		})
-	end,
+        -- 💾 Autoformateo al guardar archivos Go
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+            end,
+        })
+    end,
 }
