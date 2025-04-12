2a4305323ab09279a6097e17b9dc7cc808fa7dbf
Daniel Gonzalez <danielgona2002@gmail.com>
2025-04-12 09:04:30 -0500

    mas cambios a plugins

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,26 +1,34 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        require("neo-tree").setup({
-            window = {
-                position = "float", -- ventana flotante en vez de a la izquierda/derecha
-                popup = {
-                    size = {
-                        height = "80%",
-                        width = "50%",
-                    },
-                    border = "solid",
-                    position = "50%", -- centrado
-                },
-            },
-        })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- mostrar archivos ocultos
+					show_hidden_count = true,
+					hide_dotfiles = false, -- NO ocultar los archivos que empiezan con `.`
+					hide_gitignored = false, -- opcional: mostrar incluso los ignorados por git
+				},
+			},
+			window = {
+				position = "float",
+				popup = {
+					size = {
+						height = "80%",
+						width = "50%",
+					},
+					position = "50%",
+					border = "solid",
+				},
+			},
+		})
 
-        vim.api.nvim_set_keymap("n", "<C-b>", ":Neotree toggle<CR>", { noremap = true, silent = true })
-    end,
+		vim.api.nvim_set_keymap("n", "<C-b>", ":Neotree toggle<CR>", { noremap = true, silent = true })
+	end,
 }
