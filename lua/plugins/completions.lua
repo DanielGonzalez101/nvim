cff30300acb7aca7b0df593528d04f5dac5a8d50
Daniel Gonzalez Navarro <daniel@MacBook-Air-de-Daniel.local>
2026-02-20 12:11:41 -0500

    Actualizo mi configuracion de nvim

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -9,6 +9,12 @@ return {
 		"hrsh7th/cmp-path",
 	},
 	{
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+	},
+	{
+		"hrsh7th/cmp-cmdline",
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
@@ -16,14 +22,41 @@ return {
 		},
 		config = function()
 			local luasnip = require("luasnip")
+
+			-- Primero los snippets de friendly-snippets
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			-- ✅ CAMBIO IMPORTANTE: usa load() en vez de lazy_load()
+			require("luasnip.loaders.from_vscode").load({
+				paths = { vim.fn.stdpath("config") .. "/snippets" },
+			})
 
 			-- Configuración adicional de LuaSnip
 			luasnip.config.set_config({
 				history = true,
 				updateevents = "TextChanged,TextChangedI",
 				enable_autosnippets = true,
+				region_check_events = "CursorMoved",
 			})
+
+			-- Keymaps adicionales para snippets
+			vim.keymap.set({ "i", "s" }, "<C-k>", function()
+				if luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				end
+			end, { silent = true, desc = "Expand or jump snippet" })
+
+			vim.keymap.set({ "i", "s" }, "<C-j>", function()
+				if luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				end
+			end, { silent = true, desc = "Jump back in snippet" })
+
+			vim.keymap.set({ "i", "s" }, "<C-l>", function()
+				if luasnip.choice_active() then
+					luasnip.change_choice(1)
+				end
+			end, { silent = true, desc = "Change choice in snippet" })
 		end,
 	},
 	{
@@ -33,6 +66,8 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-cmdline",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 		},
@@ -88,17 +123,14 @@ return {
 				formatting = {
 					fields = { "kind", "abbr", "menu" },
 					format = function(entry, vim_item)
-						-- Añadir íconos
 						vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
-
-						-- Origen del completado
 						vim_item.menu = ({
 							luasnip = "[Snippet]",
 							nvim_lsp = "[LSP]",
+							nvim_lsp_signature_help = "[Signature]",
 							buffer = "[Buffer]",
 							path = "[Path]",
 						})[entry.source.name]
-
 						return vim_item
 					end,
 				},
@@ -130,19 +162,37 @@ return {
 						select = true,
 					}),
 				}),
-				-- ✅ PRIORIDAD: Snippets primero, luego LSP
 				sources = cmp.config.sources({
-					{ name = "luasnip", priority = 1000 }, -- Máxima prioridad
-					{ name = "nvim_lsp", priority = 750 },
-					{ name = "path", priority = 500 },
+					{ name = "luasnip", priority = 1000, max_item_count = 10 },
+					{ name = "nvim_lsp", priority = 750, max_item_count = 20 },
+					{ name = "nvim_lsp_signature_help", priority = 800 },
+					{ name = "path", priority = 500, max_item_count = 10 },
 				}, {
-					{ name = "buffer", keyword_length = 3, priority = 250 },
+					{ name = "buffer", keyword_length = 3, priority = 250, max_item_count = 5 },
 				}),
-				-- Performance
 				performance = {
 					debounce = 150,
 					throttle = 60,
 					fetching_timeout = 200,
+				},
+				experimental = {
+					ghost_text = false,
+				},
+			})
+
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
 				},
 			})
 		end,
